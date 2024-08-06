0185a968a421a937cbd016b60c99a662125cf7cc
Tiago-Fogolin <ticofogolin@gmail.com>
2024-08-06 17:22:09 -0300

    Finished basic circular layout

--- layouts/layouts.py
+++ layouts/layouts.py
@@ -1,9 +1,10 @@
 import random
 from functools import partial
+import math
 
 class LayoutTemplate:
 
-    def get_positions(self, nodes): raise NotImplementedError()
+    def generate_positions(self, nodes): raise NotImplementedError()
 
 class RandomLayout(LayoutTemplate):
 
@@ -24,11 +25,64 @@ class CircularLayout(LayoutTemplate):
 
     def __init__(self) -> None:
         super().__init__()
-        self.radius = 5 
+        self.radius = 200
+        self.angle = 90
         self.center_x = 800
         self.center_y = 400
         self.positions = {}
 
-    def get_positions(self, nodes):
+    def generate_positions(self, nodes):
+        next_mirrored = False
+        visited_positions = []
+        quadrants = [(1,1), (1,-1), (-1,-1), (-1,1)]
+        quadrant_index = 0
+
         for i, node in enumerate(nodes):
-           ...
+            while True:
+                # get the sign based on the quadrant rotation
+                x_sign = quadrants[quadrant_index][0]
+                y_sign = quadrants[quadrant_index][1]
+
+                # get next position based on the radius
+                next_pos = [self.radius*math.cos(math.radians(self.angle)), self.radius*math.sin(math.radians(self.angle))]
+                
+                #if its a mirrored iteration, mirror the position
+                if next_mirrored:
+                    next_pos[0], next_pos[1] = next_pos[1], next_pos[0]
+
+                # get the next X and y, based on offsets and signs
+                next_x = x_sign * next_pos[0] + self.center_x
+                next_y = y_sign * next_pos[1] + self.center_y
+
+                if [next_x, next_y] not in visited_positions:
+                    break
+
+                quadrant_index += 1
+
+                if quadrant_index == 4:
+                    quadrant_index = 0
+                    # if it was not the mirrored iteration, set the next one
+                    # to be the mirrored
+                    if not next_mirrored:
+                        if (next_pos[0], next_pos[1]) != (next_pos[1], next_pos[0]):
+                            next_mirrored = True
+                            continue
+
+                    if next_mirrored:
+                        next_mirrored = False
+
+                    # if its already been the mirrored iteration
+                    # or there was no mirror, divide the angle by two
+                    self.angle /= 2
+                
+
+            visited_positions.append([next_x, next_y])
+
+            self.positions[node.label] = {'x': next_x, 'y': next_y, 'index': i}
+
+           
+       
+        return self.positions
+    
+    
+                
